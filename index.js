c405b3632ac05544918620d1d35b0d13b4262c24
Shawn Inder <shawninder@gmail.com>
2023-03-22 18:16:51 -0400

    Add a second extended schema and see which orders work or don't

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 const z = require('zod')
 
-const baseSchema = z.object({})
+const baseSchema = z.object({
+  _: z.string()
+})
 
-const extendedSchema = baseSchema.extend({
+const aSchema = baseSchema.extend({
   a: z.string()
 })
 
-const optionsSchema = baseSchema.or(extendedSchema)
+const bSchema = baseSchema.extend({
+  b: z.string()
+})
 
-const options = {
+const _ab = baseSchema.or(aSchema).or(bSchema) // 0/2
+const _ba = baseSchema.or(bSchema).or(aSchema) // 0/2
+const a_b = aSchema.or(baseSchema).or(bSchema) // 1/2
+const b_a = bSchema.or(baseSchema).or(aSchema) // 1/2
+const ab_ = aSchema.or(bSchema).or(baseSchema) // 2/2
+const ba_ = bSchema.or(aSchema).or(baseSchema) // 2/2
+
+
+const options_A = {
+  _: '_',
   a: 'A'
 }
-
-console.log('before `schema.parse`', JSON.stringify(options))      // prints: before `schema.parse` {"a":"A"}
-const validOptions = optionsSchema.parse(options)
-console.log('-after `schema.parse`', JSON.stringify(validOptions)) // prints: -after `schema.parse` {}
+const options_B = {
+  _: '_',
+  b: 'B'
+}
+console.table([{
+  schema: 'expected',
+  a: JSON.stringify(options_A),                         // {"_":"_","a":"A"} => EXPECTED
+  b: JSON.stringify(options_B)                          // {"_":"_","b":"B"} => EXPECTED
+}, {
+  schema: '_ab',
+  a: JSON.stringify(_ab.parse(options_A)),  // {"_":"_"}         => FAIL
+  b: JSON.stringify(_ab.parse(options_B))   // {"_":"_"}         => FAIL
+},
+{
+  schema: '_ba',
+  a: JSON.stringify(_ba.parse(options_A)),  // {"_":"_"}         => FAIL
+  b: JSON.stringify(_ba.parse(options_B))   // {"_":"_"}         => FAIL
+},
+{
+  schema: 'a_b',
+  a: JSON.stringify(a_b.parse(options_A)),  // {"_":"_","a":"A"} => SUCCESS
+  b: JSON.stringify(a_b.parse(options_B))   // {"_":"_"}         => FAIL
+},
+{
+  schema: 'b_a',
+  a: JSON.stringify(b_a.parse(options_A)),  // {"_":"_"}         => FAIL
+  b: JSON.stringify(b_a.parse(options_B))   // {"_":"_","b":"B"} => SUCCESS
+},
+{
+  schema: 'ab_',
+  a: JSON.stringify(ab_.parse(options_A)),  // {"_":"_","a":"A"} => SUCCESS
+  b: JSON.stringify(ab_.parse(options_B))   // {"_":"_","b":"B"} => SUCCESS
+},
+{
+  schema: 'ba_',
+  a: JSON.stringify(ba_.parse(options_A)),  // {"_":"_","a":"A"} => SUCCESS
+  b: JSON.stringify(ba_.parse(options_B))   // {"_":"_","b":"B"} => SUCCESS
+}])
